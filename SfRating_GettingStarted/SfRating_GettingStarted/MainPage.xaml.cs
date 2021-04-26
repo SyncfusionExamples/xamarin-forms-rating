c44062826ab010f6a94e136f63f2eb1b4638fccb
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-26 17:58:54 +0530

    Getting started sample has been added

---
+++ SfRating_GettingStarted/SfRating_GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SfRating_GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            rating.ValueChanged += Rating_ValueChanged;
+        }
+
+        private void Rating_ValueChanged(object sender, Syncfusion.SfRating.XForms.ValueEventArgs e)
+        {
+            ratingLabel.Text = "Rating :" + " " + e.Value.ToString() + "/5";
+        }
+    }
+}
